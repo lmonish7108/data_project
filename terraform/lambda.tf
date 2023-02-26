eb8336e23863ebf2e513a964e31915707cba3ae0
Monish <monish_manojkumar_lalchandani@epam.com>
2023-02-26 12:49:52 +0000

    Lambda function

---
+++ terraform/lambda.tf
@@ -0,0 +1,22 @@
+data "archive_file" "lambdazip" {
+  type        = "zip"
+
+  source_dir = "${path.module}/lambda"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "push_to_kinesis" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function.zip"
+  function_name = "push_to_kinesis"
+  role          = aws_iam_role.lambda_data_project_role.arn
+  handler       = "lambda_function.lambda_handler"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = data.archive_file.lambdazip.output_base64sha256
+
+  runtime = "python3.8"
+}
