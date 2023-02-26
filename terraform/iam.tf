ca4475bad195127629d6ebd6c8e0f4c1b09fe0df
Monish <monish_manojkumar_lalchandani@epam.com>
2023-02-26 09:53:48 +0000

    Minor fixes

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -36,9 +36,7 @@ resource "local_file" "lambda_policy" {
   filename = "${path.module}/parsed_lambda.json"
 }
 
-module "iam_policy_module" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
-
+resource "aws_iam_policy" "iam_admin_policy_module" {
   name        = "t_iam_admin"
   path        = "/"
   description = "Customer managed policy, should be deleted soon"
@@ -46,9 +44,7 @@ module "iam_policy_module" {
   policy = local_file.iam_policy.content
 }
 
-module "cloudwatch_logs_policy_module" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
-
+resource "aws_iam_policy" "cloudwatch_logs_policy_module" {
   name        = "t_cloudwatch_logs_admin"
   path        = "/"
   description = "Customer managed policy, should be deleted soon"
@@ -56,9 +52,7 @@ module "cloudwatch_logs_policy_module" {
   policy = local_file.cloudwatch_logs_policy.content
 }
 
-module "firehose_policy_module" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
-
+resource "aws_iam_policy" "firehose_policy_module" {
   name        = "t_s3_admin"
   path        = "/"
   description = "Customer managed policy, should be deleted soon"
@@ -66,9 +60,7 @@ module "firehose_policy_module" {
   policy = local_file.firehose_policy.content
 }
 
-module "s3_policy_module" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
-
+resource "aws_iam_policy" "s3_policy_module" {
   name        = "t_lambda_admin"
   path        = "/"
   description = "Customer managed policy, should be deleted soon"
@@ -76,12 +68,78 @@ module "s3_policy_module" {
   policy = local_file.s3_policy.content
 }
 
-module "lambda_policy_module" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
-
+resource "aws_iam_policy" "lambda_policy_module" {
   name        = "t_firehose_admin"
   path        = "/"
   description = "Customer managed policy, should be deleted soon"
 
   policy = local_file.lambda_policy.content
+}
+
+resource "aws_iam_role" "lambda_data_project_role" {
+  name        = "lambda_data_project_role"
+  description = "Customer managed, should be deleted. Gives permissions to lambda for various data tasks."
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_iam_attachment" {
+  role       = aws_iam_role.lambda_data_project_role.name
+  policy_arn = aws_iam_policy.iam_admin_policy_module.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_s3_attachment" {
+  role       = aws_iam_role.lambda_data_project_role.name
+  policy_arn = aws_iam_policy.s3_policy_module.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_cloudwatch_attachment" {
+  role       = aws_iam_role.lambda_data_project_role.name
+  policy_arn = aws_iam_policy.cloudwatch_logs_policy_module.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_lambda_attachment" {
+  role       = aws_iam_role.lambda_data_project_role.name
+  policy_arn = aws_iam_policy.lambda_policy_module.arn
+}
+
+resource "aws_iam_group" "user_group" {
+  name = "data_project_user_group"
+}
+
+resource "aws_iam_group_policy_attachment" "group_iam_attachment" {
+  group       = aws_iam_group.user_group.name
+  policy_arn = aws_iam_policy.iam_admin_policy_module.arn
+}
+
+resource "aws_iam_group_policy_attachment" "group_s3_attachment" {
+  group       = aws_iam_group.user_group.name
+  policy_arn = aws_iam_policy.s3_policy_module.arn
+}
+
+resource "aws_iam_group_policy_attachment" "group_cloudwatch_attachment" {
+  group       = aws_iam_group.user_group.name
+  policy_arn = aws_iam_policy.cloudwatch_logs_policy_module.arn
+}
+
+resource "aws_iam_group_policy_attachment" "group_lambda_attachment" {
+  group       = aws_iam_group.user_group.name
+  policy_arn = aws_iam_policy.lambda_policy_module.arn
+}
+
+resource "aws_iam_group_policy_attachment" "group_firehose_attachment" {
+  group       = aws_iam_group.user_group.name
+  policy_arn = aws_iam_policy.firehose_policy_module.arn
 }
